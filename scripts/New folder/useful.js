9a4b539ea547e34bebcf9e206dab89da8310535f
MartinKostadinov <beastdubaka@gmail.com>
2017-05-30 23:39:26 +0300

    fix

--- scripts/New folder/useful.js
+++ scripts/New folder/useful.js
@@ -56,3 +56,71 @@
 
     });
 }(jQuery));
+(function ($) {
+    $(document).ready(function () {
+        'use strict';
+        var carusel = (function () {
+           var currentSlide = 0;
+           var Carusel = function (slideItems, slideClass, slideActiveClass, bulletItems, bulletClass, bulletActiveClass) {
+                this.slideItems = slideItems;
+                this.slideClass = slideClass;
+                this.slideActiveClass = slideActiveClass;
+                this.bulletItems = bulletItems;
+                this.bulletClass = bulletClass;
+                this.bulletActiveClass = bulletActiveClass;
+           
+            };
+
+            Carusel.prototype.nextSlide = function () {
+                this.changeSlide(currentSlide + 1);
+            };
+            Carusel.prototype.previousSlide = function () {
+                this.changeSlide(currentSlide - 1);
+            };
+            
+            Carusel.prototype.changeSlide = function (n) {
+                this.slideItems[currentSlide].className = slideClass;
+                this.bulletItems[currentSlide].className = bulletClass;
+                currentSlide = (n + this.slideItems.length) % $slideItems.length;
+                this.slideItems[currentSlide].className = slideClass + ' ' + slideActiveClass;
+                this.bulletItems[currentSlide].className = bulletClass + ' ' + bulletActiveClass;
+            };
+
+            return Carusel;
+        }());
+        var
+            $caruselCont = $('#main-gallery'),
+            $mainGallery = $('#carusel'),//ul
+            $slideItems = $mainGallery.children('.slide'),//li
+            slideClass = 'slide',
+            slideActiveClass = 'js-slide-visible',
+            $rightSlideButton = $caruselCont.find('#right-side-button'),
+            $leftSlideButton = $caruselCont.find('#left-side-button'),
+            $bullets = $('.bullets').children('.bullet'),
+            bulletClass = 'bullet',
+            bulletActiveClass = 'active';
+
+        var firstCarusel = new carusel($slideItems, slideClass, slideActiveClass, $bullets, bulletClass, bulletActiveClass);
+        //auto change  the images
+        var slideInterval = setInterval(function (){
+            firstCarusel.nextSlide();
+        }, 1500);
+
+            $('#right-side-button').on('click ', function () {
+                firstCarusel.nextSlide();
+                clearInterval(slideInterval);
+            });
+            $('#left-side-button').on('click ', function () {
+                firstCarusel.previousSlide();
+                clearInterval(slideInterval);
+            });
+
+            $('.bullets').on('click','.bullet', function (){
+                    var $t = $(this).index('.bullet');
+                firstCarusel.changeSlide($t);
+                clearInterval(slideInterval);
+            });
+
+        });
+    }(jQuery));
+
